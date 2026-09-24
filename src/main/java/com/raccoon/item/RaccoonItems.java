663571d63620f280e5d4e1926f6c70c4acc6bef5
Dave - Pulstack <dave@pulstack.com>
2026-09-23 17:36:59 -0700

    maybe a raccoon thing

--- src/main/java/com/raccoon/item/RaccoonItems.java
+++ src/main/java/com/raccoon/item/RaccoonItems.java
@@ -4,6 +4,7 @@ import com.raccoon.RaccoonMod;
 import com.raccoon.entity.RaccoonEntities;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
+import net.neoforged.neoforge.common.DeferredSpawnEggItem;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -11,5 +12,5 @@ public class RaccoonItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(RaccoonMod.MOD_ID);
 
     public static final DeferredItem<SpawnEggItem> RACCOON_SPAWN_EGG = ITEMS.register("raccoon_spawn_egg",
-            () -> new SpawnEggItem(RaccoonEntities.RACCOON.get(), 0x5C5856, 0x1E1E1E, new Item.Properties()));
+            () -> new DeferredSpawnEggItem(RaccoonEntities.RACCOON, 0x5C5856, 0x1E1E1E, new Item.Properties()));
 }
